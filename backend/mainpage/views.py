8ded58eb37ec158f63af13b3b99a9dfbb89eecb3
Joshua <jpe7619@gmail.com>
2024-01-04 14:05:44 -0800

    update CORS, establish backend photo hosting, test front end call

--- backend/mainpage/views.py
+++ backend/mainpage/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Item
 
-# Create your views here.
+
+def get_all_photos(request):
+    photos = Item.objects.all().values("photo")
+    photo_urls = [photo["photo"] for photo in photos if photo["photo"]]
+    return JsonResponse({"photoUrls": photo_urls}, safe=False)
